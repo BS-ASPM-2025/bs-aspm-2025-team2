38ad14a2b406c600bfd71f72f97613afc5a9bc36
marialish-dotcom <masha.ivan2007@gmail.com>
2026-01-14 14:55:21 +0200

    Create CandidateControllerTest.java

---
+++ backend/src/test/java/bsaspm2025team2/backend/CandidateControllerTest.java
@@ -0,0 +1,12 @@
+package bsaspm2025team2.backend;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class CandidateControllerTest {
+
+    @Test
+    void dummyTest() {
+        assertTrue(true);
+    }
+}
